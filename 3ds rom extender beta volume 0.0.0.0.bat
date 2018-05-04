a3f82c90711bf8a1d12f7a2e27457e885811c75f
Glsmaster <38990172+Glsmaster@users.noreply.github.com>
2018-05-04 21:31:43 +0200

    Add files via upload

---
+++ 3ds rom extender beta volume 0.0.0.0.bat
@@ -0,0 +1,71 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFA5HSRa+GGStCLkT6ezo0+6CtkgeQOspfcLL07+PbukQ5SU=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSTk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFA5HSRa+GGS5E7gZ5vzo07mDsQ0PW+9/fZfP37qKJfJT/kD3YdYh129Um8MJHlVdZhfL
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+BIENVENUE DANS 3DS ROM EXTENDER BETA V0.0.0.0 DE GLS
+Ouvrir rom : Detective Pikachu (USA) citra.3ds
+la rom est valide
+la rom n'a pas besoin d'etre byte swappe
+decompression et deplacement des fuck0o fichiers
+00000000
+00000001
+00000002
+00000003
+00000004
+00000005
+00000006
+00000007
+00000008
+remappage des points
+00000000
+00000001
+00000002
+00000003
+00000004
+00000005
+00000006
+00000007
+00000008
+recalculage checksum
+00000000
+00000001
+00000002
+00000003
+00000004
+00000005
+00000006
+00000007
+00000008
+sauvegarde du point special
+fin
+rom interieur : Detective Pikachu (USA) citra.3ds
+rom exterieur : Detective Pikachu (USA) citra.ext.3ds
